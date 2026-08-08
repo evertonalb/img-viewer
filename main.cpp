aa79da694b9c3e256357984c07c7cf59ab516f1b
Everton Albuquerque de Oliveira <186333879+evertonalb@users.noreply.github.com>
2026-08-08 12:54:33 -0400

    Make marker identification more specific

--- main.cpp
+++ main.cpp
@@ -8,14 +8,30 @@
 int next_byte(FILE* fp, unsigned char& byte);
 
 int main(int argc, char* argv[]){
-    FILE* fp = fopen("huff_simple0.jpg", "r");
+    FILE* fp = fopen("examples/colored_8x8.jpg", "r");
 
+    // Detect markers
     unsigned char byte;
     while (next_byte(fp, byte)){
-        if (byte == 0xff){
+        if (byte == 0xff){ // potential marker
             next_byte(fp, byte);
-            if (byte != 0x00)
+            if (byte != 0x00){
                 fprintf(stdout, "Spotted marker 0xff%02x.\n", byte);
+                switch (byte){
+                case 0xd8:
+                    fprintf(stdout, "Start-of-image marker.\n");
+                    break;
+                case 0xd9:
+                    fprintf(stdout, "End-of-image marker.\n");
+                    break;
+                case 0xda:
+                    fprintf(stdout, "Start-of-scan marker.\n");
+                    break;
+                default:
+                    fprintf(stdout, "This marker has not been implemented yet.\n");
+                    break;
+                }
+            }
         }
     }
 
