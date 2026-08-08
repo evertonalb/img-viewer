ec4f71b22c4935ff19f10d053e884c044c4cc71a
Everton Albuquerque de Oliveira <186333879+evertonalb@users.noreply.github.com>
2026-08-07 23:20:56 -0400

    Refactor byte-spitter and close a file

--- tools/byte-spitter.c
+++ tools/byte-spitter.c
@@ -1,12 +1,26 @@
 // byte-spitter.c
 // Reads in a file and spits out all its bytes in order.
 // Author: Everton Albuquerque de Oliveira
-// Aug 6, 2026
+// Aug 7, 2026
 
 #include <stdio.h>
 
+// Prints out to stdout every byte of the fp file.
+void spit_bytes(FILE* fp){
+    unsigned char buffer[2]; // unsigned so that the MSB doesn't get extended
+    int c = 20; // counter for where to put the new line
+    while (fgets(buffer, 2, fp)){
+        if (c == 0){
+            fprintf(stdout, "\n");
+            c = 20;
+        }
+        fprintf(stdout, "%02x ", buffer[0]); // print out one byte
+        c--;
+    }
+}
+
 int main(int argc, char* argv[]){
-    // Only allow for one cmd-line argument
+    // Only allow for only one cmd-line argument
     if (argc < 2 || argc > 2){
         fprintf(stderr, "Usage: %s [FILENAME]\n", argv[0]);
         return 1;
@@ -19,16 +33,9 @@ int main(int argc, char* argv[]){
         return 1;
     }
 
-    unsigned char buffer[2]; // unsigned so that the MSB doesn't get extended
-    int c = 20; // counter for where to put the new line
-    while (fgets(buffer, 2, fp)){
-        if (c == 0){
-            fprintf(stdout, "\n");
-            c = 20;
-        }
-        fprintf(stdout, "%02x ", buffer[0]); // print out one byte
-        c--;
-    }
+    spit_bytes(fp);
+
     fprintf(stdout, "\n");
+    fclose(fp);
     return 0;
 }
