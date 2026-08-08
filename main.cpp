ce225530203dedf9278256364a6afa0bbe98bf47
Everton Albuquerque de Oliveira <186333879+evertonalb@users.noreply.github.com>
2026-08-07 23:37:49 -0400

    Implement next_byte function
    The function retrieves the next byte of a given file.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 // main.cpp
 // Author: Everton Albuquerque de Oliveira
 #include <iostream>
+#include <cstdio>
+
+// Saves next byte of fp into "byte".
+// Returns 0 if the file ended and 1 otherwise.
+int next_byte(FILE* fp, unsigned char& byte);
 
 int main(int argc, char* argv[]){
-    std::cout << "Hello, World!" << std::endl;
+    FILE* fp = fopen("foo", "r");
+    unsigned char byte;
+    next_byte(fp, byte);
+    fprintf(stdout, "%02x\n", byte);
     return 0;
+}
+
+int next_byte(FILE* fp, unsigned char& byte){
+    char buffer[2];
+    if (fgets(buffer, 2, fp)){
+        byte = buffer[0];
+        return 1;
+    } else return 0;
 }
