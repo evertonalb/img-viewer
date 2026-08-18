e2781542627f46c82a4e7586c8ec9a23afea2223
Everton Albuquerque de Oliveira <186333879+evertonalb@users.noreply.github.com>
2026-08-18 15:01:10 -0400

    Add function to retrieve next two bytes at once

--- inc/util.hpp
+++ inc/util.hpp
@@ -1,3 +1,6 @@
+// util.hpp
+// Authur: Everton Albuquerque de Oliveira
+// Aug 17, 2026
 #pragma once
 
 #include <cstdio>
@@ -5,3 +8,7 @@
 // Saves next byte of fp into "byte".
 // Returns 0 if the file ended and 1 otherwise.
 int next_byte(FILE* fp, unsigned char& byte);
+
+// Saves next two bytes of fp into twoBytes
+// Returns number of bytes read.
+int next_two_bytes(FILE* fp, int& twoBytes);
--- src/util.cpp
+++ src/util.cpp
@@ -1,3 +1,6 @@
+// util.cpp
+// Authur: Everton Albuquerque de Oliveira
+// Aug 17, 2026
 #include "util.hpp"
 
 int next_byte(FILE* fp, unsigned char& byte){
@@ -6,4 +9,20 @@ int next_byte(FILE* fp, unsigned char& byte){
         byte = buffer[0];
         return 1;
     } else return 0;
+}
+
+int next_two_bytes(FILE* fp, int& twoBytes){
+    unsigned char byte;
+    if (!next_byte(fp, byte)){
+        return 0;
+    }
+
+    twoBytes = byte;
+
+    if (!next_byte(fp, byte)){
+        return 1;
+    }
+
+    twoBytes = (twoBytes << 8) | byte;
+    return 2;
 }
